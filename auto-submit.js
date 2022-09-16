49d57d0dbb63ee59160e995be6af71b8fcb53e0e
fangqk1991 <me@fangqk.com>
2022-09-16 09:37:51 +0800

    +schedule

--- auto-submit.js
+++ auto-submit.js
@@ -1,8 +1,25 @@
+const sleep = (ms) =>{
+  return new Promise((resolve) => setTimeout(resolve, ms))
+}
+
 const btn = document.querySelector('.go-btn')
+
+const targetTime = new Date()
+targetTime.setUTCHours(2)
+targetTime.setMinutes(0)
+targetTime.setSeconds(0)
+const targetTs = targetTime.getTime()
+
+const waitToReload = async () => {
+  while (Date.now() < targetTs) {
+    console.info(`sleep.`)
+    await sleep(100)
+  }
+  window.location.reload()
+}
+
 if (btn) {
-  console.info(`click go-btn`)
   btn.click()
 } else {
-  console.info(`reload page`)
-  window.location.reload()
+  waitToReload()
 }
