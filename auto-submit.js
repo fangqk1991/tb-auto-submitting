e33da0e30dd4f991786cba93022f8715f719fead
Fang <me@fangqk.com>
2022-09-18 21:09:05 +0800

    update auto-submit.js

--- auto-submit.js
+++ auto-submit.js
@@ -2,24 +2,24 @@ const sleep = (ms) =>{
   return new Promise((resolve) => setTimeout(resolve, ms))
 }
 
-const btn = document.querySelector('.go-btn')
+const main = async () => {
+  const targetTime = new Date()
+  targetTime.setUTCHours(2)
+  targetTime.setMinutes(0)
+  targetTime.setSeconds(0)
+  const targetTs = targetTime.getTime()
 
-const targetTime = new Date()
-targetTime.setUTCHours(2)
-targetTime.setMinutes(0)
-targetTime.setSeconds(0)
-const targetTs = targetTime.getTime()
-
-const waitToReload = async () => {
-  while (Date.now() < targetTs) {
-    console.info(`sleep.`)
-    await sleep(100)
+  const btn = document.querySelector('.go-btn')
+  if (btn) {
+    await sleep(200)
+    btn.click()
+  } else {
+    while (Date.now() < targetTs) {
+      console.info(`sleep.`)
+      await sleep(100)
+    }
+    window.location.reload()
   }
-  window.location.reload()
 }
 
-if (btn) {
-  btn.click()
-} else {
-  waitToReload()
-}
+main()
